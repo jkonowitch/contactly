92168b1f8a67b7378f15cc4ac9a8c7caedc8902d
jkonowitch <jkonowitch@gmail.com>
2015-01-15 12:18:48 -0500

    connect

--- db/connection.rb
+++ db/connection.rb
@@ -1,5 +1,5 @@
 require 'active_record'
 
-ActiveRecord::Base.establish_connection('postgresql://jeff@127.0.0.1/contact_list')
+ActiveRecord::Base.establish_connection("postgresql://#{ENV["DB_INFO"]}@127.0.0.1/contact_list")
 
 ActiveRecord::Base.logger = Logger.new(STDOUT)
